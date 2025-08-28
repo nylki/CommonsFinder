b951902452f564da1cae24407c4ea885fd2fe412
Tom Brewe <tom@nylkiway.net>
2025-08-28 17:34:16 +0200

    feat: experimental falback gradient for pure commons categories without main image

--- CommonsFinder/Views/Reusable Views/CategoryTeaserBackground.swift
+++ CommonsFinder/Views/Reusable Views/CategoryTeaserBackground.swift
@@ -24,18 +24,20 @@ struct CategoryTeaserBackground: View {
                             Color.clear
                         }
                     }
+                } else {
+                    ProceduralBackground(categoryName: category.commonsCategory ?? category.wikidataId ?? "")
+                        .blur(radius: 5)
+                        .clipped()
                 }
             }
             .overlay {
-                if category.thumbnailImage != nil {
-                    LinearGradient(
-                        stops: [
-                            .init(color: .init(white: 0, opacity: 0), location: 0),
-                            .init(color: .init(white: 0, opacity: 0.1), location: 0.35),
-                            .init(color: .init(white: 0, opacity: 0.2), location: 0.5),
-                            .init(color: .init(white: 0, opacity: 0.8), location: 1),
-                        ], startPoint: .top, endPoint: .bottom)
-                }
+                LinearGradient(
+                    stops: [
+                        .init(color: .init(white: 0, opacity: 0), location: 0),
+                        .init(color: .init(white: 0, opacity: 0.1), location: 0.35),
+                        .init(color: .init(white: 0, opacity: 0.2), location: 0.5),
+                        .init(color: .init(white: 0, opacity: 0.8), location: 1),
+                    ], startPoint: .top, endPoint: .bottom)
             }
     }
 }
