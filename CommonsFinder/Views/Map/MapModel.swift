68af4b2ad59ec1620f7b7d83a43035466b24aacc
Tom Brewe <tom@nylkiway.net>
2025-12-11 13:51:34 +0100

    fix: fixes a regression when switching the map item type
    and separate the WrappedMapStyle into its own file

--- CommonsFinder/Views/Map/MapModel.swift
+++ CommonsFinder/Views/Map/MapModel.swift
@@ -19,42 +19,6 @@ enum MapError: Error {
     case itemWithoutCoordinateCannotBeShownOnMap
 }
 
-enum WrappedMapStyle: String, CaseIterable, Identifiable {
-    case standard
-    case satellite
-
-    var id: String {
-        self.rawValue
-    }
-
-    static var allCases: [WrappedMapStyle] = [
-        .standard, .satellite,
-    ]
-
-    var asMKMapStyle: MapStyle {
-        switch self {
-        case .standard:
-            .standard(
-                elevation: .automatic,
-                emphasis: .automatic,
-                pointsOfInterest: .excludingAll,
-                showsTraffic: false
-            )
-        case .satellite:
-            .hybrid(elevation: .realistic, pointsOfInterest: .excludingAll, showsTraffic: false)
-        }
-    }
-
-    var labelText: String {
-        switch self {
-        case .standard:
-            String(localized: "Standard", comment: "standard map style")
-        case .satellite:
-            String(localized: "Satellite", comment: "satellite map style")
-        }
-    }
-}
-
 @Observable final class MapModel {
     var position: MapCameraPosition = .automatic
     private(set) var region: MKCoordinateRegion?
@@ -163,6 +127,7 @@ enum WrappedMapStyle: String, CaseIterable, Identifiable {
         } else {
             selectedMapItem?.mapSheetFocusedItem = .init()
             presentedMapSheet = nil
+            fetchDataForCurrentRegion(shouldDebounce: false)
         }
     }
 
