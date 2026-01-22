b44354ee4ba1ea891dba70a65b70bd809750d10a
Tom Brewe <tom@nylkiway.net>
2026-01-22 15:25:19 +0100

    fix(API): add uselang:content to most action api calls, to supposedly help server caching

---
+++ CommonsFinder/Views/Reusable Views/DeepCategoryToggle.swift
@@ -0,0 +1,18 @@
+//
+//  DeepCategoryToggle.swift
+//  CommonsFinder
+//
+//  Created by Tom Brewe on 22.01.26.
+//
+
+import SwiftUI
+
+struct DeepCategoryToggle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeepCategoryToggle()
+}
---
+++ CommonsFinder/Views/Reusable Views/SearchOrderButton.swift
@@ -0,0 +1,18 @@
+//
+//  SearchOrderButton.swift
+//  CommonsFinder
+//
+//  Created by Tom Brewe on 22.01.26.
+//
+
+import SwiftUI
+
+struct SearchOrderButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchOrderButton()
+}
